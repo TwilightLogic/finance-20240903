2bfe3e311c7cea23dc7a3d9390f6ee8d95102e9b
logic <twilightlogic@gmail.com>
2024-09-05 01:47:15 +0800

    feat: add middleware

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,15 +1,45 @@
+import { z } from "zod";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+
+import authors from "./authors";
+import books from "./books";
+
+import { zValidator } from "@hono/zod-validator";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c) => {
-  return c.json({
-    message: "Hello Next.js!",
-  });
-});
+// 我们会在 /api/hello 路由中使用 Clerk 进行身份验证(middleware)
+// app.get(
+//   // ROUTE
+//   "/hello",
+//   // MIDDLEWARE
+//   clerkMiddleware(),
+//   // CONTROLLER
+//   (c) => {
+//     const auth = getAuth(c);
+
+//     if (!auth?.userId) {
+//       return c.json(
+//         {
+//           message: "Unauthorized",
+//         },
+//         401
+//       );
+//     }
+
+//     return c.json({
+//       userId: auth.userId,
+//       message: `Hello ${auth.userId}!`,
+//     });
+//   }
+// );
+
+app.route("/authors", authors);
+app.route("/books", books);
 
 export const GET = handle(app);
 export const POST = handle(app);
