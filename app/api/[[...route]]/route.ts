678b332ea953671e36cfc97fc4f69ba941c0eeb8
logic <twilightlogic@gmail.com>
2024-09-05 14:39:08 +0800

    chore: revert to the old method for calling an api

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
-import authors from "./authors";
-import books from "./books";
+// import authors from "./authors";
+// import books from "./books";
 
 import { zValidator } from "@hono/zod-validator";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
@@ -38,8 +38,10 @@ const app = new Hono().basePath("/api");
 //   }
 // );
 
-app.route("/authors", authors);
-app.route("/books", books);
+// app.route("/authors", authors);
+// app.route("/books", books);
+
+app.get("/hello", (c) => c.json({ message: "Hello World" }));
 
 export const GET = handle(app);
 export const POST = handle(app);
