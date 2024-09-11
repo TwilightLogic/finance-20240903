a3ab61b7dfd8c83cda4d72e1a25ba30883a099f7
logic <twilightlogic@gmail.com>
2024-09-12 00:49:18 +0800

    feat: add query-provider

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -8,6 +8,8 @@ import { handle } from "hono/vercel";
 import { zValidator } from "@hono/zod-validator";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 
+import accounts from "./accounts";
+
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
@@ -41,7 +43,9 @@ const app = new Hono().basePath("/api");
 // app.route("/authors", authors);
 // app.route("/books", books);
 
-app.get("/hello", (c) => c.json({ message: "Hello World" }));
+const routes = app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;
